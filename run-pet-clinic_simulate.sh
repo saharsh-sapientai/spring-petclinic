2227030f6d3d8847315a4fc27e2d049b214aa7a0
Saharsh Basal <saharsh@sapient.ai>
2025-01-14 14:54:50 +0530

    Fixed pet-clinic repo.

---
+++ run-pet-clinic_simulate.sh
@@ -0,0 +1,23 @@
+export ENABLE_EXTENSION=true;
+export EXTENSION_MODE=simulate;
+export GO_SERVER_BASE_URL=https://app.baserock.ai/it/leService;
+export MQ_HOST=localhost;
+export MQ_PORT=5673;
+export MQ_QUEUE=learning_engine_test_queue;
+export MQ_USER=user;
+export POSTGRES_DB=petclinic;
+export POSTGRES_PASSWORD=petclinic;
+export POSTGRES_USER=petclinic;
+export PUBLISHER_MODE=RabbitMQ;
+export MQ_PASSWORD=<password>
+
+#./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-javaagent:/Users/saharshbasal/Development/git/testing/spring-petclinic/src/main/resources/opentelemetry-javaagent.jar -Dotel.resource.attributes=service.name=pet-clinic-service -Dotel.javaagent.extensions=/Users/saharshbasal/Development/git/testing/spring-petclinic/src/main/resources/sapient-otel-extension-1.0-all.jar -Dotel.traces.exporter=zipkin -Dio.opentelemetry.javaagent.slf4j.simpleLogger.defaultLogLevel=off"  -Dspring-boot.run.profiles=postgres
+java -javaagent:/Users/saharshbasal/Development/git/testing/spring-petclinic/src/main/resources/opentelemetry-javaagent.jar \
+ -Dotel.resource.attributes=service.name=pet-clinic-service \
+ -Dotel.javaagent.extensions=/Users/saharshbasal/Development/git/testing/spring-petclinic/src/main/resources/sapient-otel-extension-1.0-all.jar \
+ -Dotel.traces.exporter=zipkin \
+ -Dio.opentelemetry.javaagent.slf4j.simpleLogger.defaultLogLevel=off \
+ -Dspring.profiles.active=postgres \
+ -Dlogging.level.org.springframework.boot.context.config=DEBUG \
+ -jar /Users/saharshbasal/Development/git/testing/spring-petclinic/build/libs/spring-petclinic-3.3.0.jar
+
