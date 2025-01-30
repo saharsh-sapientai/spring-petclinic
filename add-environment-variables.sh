e565acedf99a5e8e044b6563ec3f83e87daba7d6
Saharsh Basal <saharsh@sapient.ai>
2025-01-30 14:20:02 +0530

    Updating binaries and scripts.

---
+++ add-environment-variables.sh
@@ -0,0 +1,9 @@
+export PUBLISHER_MODE=RabbitMQ;
+export MQ_HOST=localhost;
+export MQ_PORT=5673;
+export MQ_QUEUE=learning_engine_test_queue;
+export MQ_USER=user;
+export MQ_PASSWORD=<password>
+export SERVICE_NAME=pet-clinic-service;
+
+./check-service-name.sh
--- run-simulation.sh
+++ run-simulation.sh
@@ -1,4 +1,7 @@
 export LE_SERVICE_URL=https://app.baserock.ai/it/leService
 export SERVICE_UNDER_TEST_URL=http://localhost:8080
+export SERVICE_NAME=pet-clinic-service
 
-./api_automation_binary pet-clinic-service ""
+./check-service-name.sh
+
+./api_automation_binary ${SERVICE_NAME} ""
