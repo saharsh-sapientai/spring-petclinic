1e16d9206a8a7b21cf6edb40ab935760e863bb2c
Saharsh Basal <saharsh@sapient.ai>
2025-03-20 14:31:16 +0530

    Revert "Added environment check."
    This reverts commit 77ecb73788920cdb27e4d8dba2eb570c434dc700.

--- run-simulation.sh
+++ run-simulation.sh
@@ -10,13 +10,4 @@ else
     echo "SERVICE_NAME is set to '$SERVICE_NAME'."
 fi
 
-# Check if ENVIRONMENT is set
-if [ -z "$ENVIRONMENT" ]; then
-    echo "Error: ENVIRONMENT is not set."
-    echo "Please set it using: export ENVIRONMENT=<environment>"
-    exit 1
-else
-    echo "ENVIRONMENT is set to '$ENVIRONMENT'."
-fi
-
 ./api_automation_binary ${SERVICE_NAME} ""
